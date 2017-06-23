0f783f6413e5c5d5e5c065f528d14bb37993c388
Timothy Place <tim@cycling74.com>
2017-06-23 08:01:32 -0600

    jit.filter.butterworth: initial

---
+++ source/projects/jit.filter.butterworth/jit.filter.butterworth.cpp
@@ -0,0 +1,28 @@
+/// @file
+/// @copyright	Copyright (c) 2017, Cycling '74
+/// @author		Timothy Place
+/// @license	Usage of this file and its contents is governed by the MIT License
+
+#include "DspFilters/Butterworth.h"
+using namespace Dsp::Butterworth::Design;
+#include "../filter.h"
+
+
+class jit_butterworth : public filter_mop<jit_butterworth> {
+public:
+
+	MIN_DESCRIPTION { "Nth-order elliptic filter" };
+	MIN_TAGS		{ "filters" };
+	MIN_AUTHOR		{ "Cycling '74" };
+	MIN_RELATED		{ "filterdesign, filterdetail, slide, filter.elliptic~" };
+
+	inlet<>		in	{ this, "(matrix) input to be filtered", "matrix" };
+	outlet<>	out	{ this, "(matrix) filtered output", "matrix" };
+
+	jit_butterworth(const atoms& args = {})
+	: filter_mop(args)
+	{}
+};
+
+
+MIN_EXTERNAL(jit_butterworth);
