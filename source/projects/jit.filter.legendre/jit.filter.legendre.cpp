ebce39dab5e8a891f3968d8f34bdaa4f08c7429c
Timothy Place <tim@cycling74.com>
2017-06-23 08:02:18 -0600

    jit.filter.legendre: initial

---
+++ source/projects/jit.filter.legendre/jit.filter.legendre.cpp
@@ -0,0 +1,28 @@
+/// @file
+/// @copyright	Copyright (c) 2017, Cycling '74
+/// @author		Timothy Place
+/// @license	Usage of this file and its contents is governed by the MIT License
+
+#include "DspFilters/Legendre.h"
+using namespace Dsp::Legendre::Design;
+#include "../filter.h"
+
+
+class jit_legendre : public filter_mop<jit_legendre> {
+public:
+
+	MIN_DESCRIPTION { "Nth-order elliptic filter" };
+	MIN_TAGS		{ "filters" };
+	MIN_AUTHOR		{ "Cycling '74" };
+	MIN_RELATED		{ "filterdesign, filterdetail, slide, filter.elliptic~" };
+
+	inlet<>		in	{ this, "(matrix) input to be filtered", "matrix" };
+	outlet<>	out	{ this, "(matrix) filtered output", "matrix" };
+
+	jit_legendre(const atoms& args = {})
+	: filter_mop(args)
+	{}
+};
+
+
+MIN_EXTERNAL(jit_legendre);
